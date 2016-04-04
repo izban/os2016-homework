71a8fc9ef632b0c9c7c4ea3edc727713bec57ff6
Ilya <izban@mail.ru>
2016-04-04 11:18:02 +0300

    seems like working

--- netsh/testSubsequent.sh
+++ netsh/testSubsequent.sh
@@ -10,5 +10,6 @@ echo "echo -e 123 45 67 \n 123 456 67" | socat STDIO TCP:localhost:$port
 echo "echo 1 | echo 1" | socat STDIO TCP:localhost:$port
 echo "echo -e 123 45 67 \r\n 123 456 67\r\n | grep 56" | socat STDIO TCP:localhost:$port 
 echo "cat /proc/cpuinfo | grep 'model name' | sed -re 's/.*: (.*)/\1/' | uniq" | socat STDIO TCP:localhost:$port
-(echo "sh"; echo "echo \"hello, I am shell\""; sleep 0.1; echo "cat"; echo "3") | socat STDIO TCP:localhost:$port
-
+(echo "sh"; echo "echo \"hello, I am shell\""; sleep 0.5; echo "echo \"I am still here\"") | socat STDIO TCP:localhost:$port
+(echo "sh"; echo "cat"; echo "I am cat, meow"; sleep 0.1) | socat STDIO TCP:localhost:$port #take care that it SHOULD NOT print "I am cat, meow" because it is how sh work
+(echo "sh"; echo "cat"; sleep 0.1; echo "I am cat, meow"; sleep 0.1) | socat STDIO TCP:localhost:$port
